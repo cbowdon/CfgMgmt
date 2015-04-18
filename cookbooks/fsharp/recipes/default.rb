5d07527406b2bdea45573649abc51e54ea5bfab4
Chris Bowdon <c.bowdon@bath.edu>
2015-04-18 22:13:57 +0100

    Ensure apt-get updated after adding PPA

--- cookbooks/fsharp/recipes/default.rb
+++ cookbooks/fsharp/recipes/default.rb
@@ -10,6 +10,7 @@ bash 'Add Xamarin Mono PPA' do
   code <<-EOH
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
   echo "deb http://download.mono-project.com/repo/debian wheezy main" | tee /etc/apt/sources.list.d/mono-xamarin.list
+  apt-get update
   EOH
 end
 
